7ab33c9931581d978dd32bbe12a7a1c8704783b8
Arsenii13 <arseniyonjegeniy@gmail.com>
2026-07-04 14:14:47 +0300

    s

--- main.py
+++ main.py
@@ -90,4 +90,4 @@ def disconnect():
     print(f"{name} left the room {room}")
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
